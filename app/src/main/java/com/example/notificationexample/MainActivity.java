a46cf83e1a83325356ef473b32c9af7da448f763
SkyRavenS1lver <revandra.aryo.dwi.krisnandaru@mail.ugm.ac.id>
2022-11-03 14:45:13 +0700

    Firat

--- app/src/main/java/com/example/notificationexample/MainActivity.java
+++ app/src/main/java/com/example/notificationexample/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.notificationexample;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     private NotificationManager notificationManager;
     private final static String CHANNEL_ID = "primary channel";
     private int NOTIFICATION_ID = 0;
+    //Pemberian nama untuk receiver
+    private final static String ACTION_UPDATE_NOTIF = "ACTION_UPDATE_NOTIF";
+    private final static String ACTION_CANCEL_NOTIF = "ACTION_CANCEL_NOTIF";
+    private  NotificationReceiver receiverRegister = new NotificationReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +43,31 @@ public class MainActivity extends AppCompatActivity {
                 sendNotification();
             }
         });
+        findViewById(R.id.update_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Menambah gambar pada notification
+                updateNotification();
+            }
+        });
+        findViewById(R.id.Cancel_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notificationManager.cancel(NOTIFICATION_ID);
+            }
+        });
+        registerReceiver(receiverRegister, new IntentFilter(ACTION_UPDATE_NOTIF));
+        registerReceiver(receiverRegister, new IntentFilter(ACTION_CANCEL_NOTIF));
     }
+
+    private void updateNotification() {
+        Bitmap androidImage = BitmapFactory.decodeResource(getResources(), R.drawable.mascot);
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notifyBuilder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(androidImage)
+        .setBigContentTitle("Notification Updated!"));
+        notificationManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+
     //Untuk set title, icon, content
     private NotificationCompat.Builder getNotificationBuilder(){
 
@@ -52,8 +85,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendNotification(){
+        Intent updateIntent = new Intent(ACTION_UPDATE_NOTIF);
+        Intent cancerIntent = new Intent(ACTION_CANCEL_NOTIF);
+        PendingIntent updatePendingIntent = PendingIntent.getBroadcast(this,
+                NOTIFICATION_ID, updateIntent, PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent cancerPendingIntent = PendingIntent.getBroadcast(this,
+                NOTIFICATION_ID, cancerIntent, PendingIntent.FLAG_IMMUTABLE);
         NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        //Penambahan Teks Untuk Notif
+        notifyBuilder.addAction(R.drawable.ic_copyright_black_24dp, "Update Notification", updatePendingIntent);
+        notifyBuilder.addAction(R.drawable.ic_copyright_black_24dp, "Delete", cancerPendingIntent);
+
         //Supaya bisa mengirim, kita butuh yang namanya notification id
         notificationManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+    //Pembuatan receiver untuk pencetan dari notification
+    //Jangan lupa tambahkan (register) di receiver
+    public class NotificationReceiver extends BroadcastReceiver{
+
+        public NotificationReceiver() {
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(ACTION_UPDATE_NOTIF)){
+                updateNotification();
+            }
+            else if (action.equals(ACTION_CANCEL_NOTIF)){
+                notificationManager.cancel(NOTIFICATION_ID);
+            }
+        }
+    }
+
+    //Jangan lupa unregister receiver onDestroy, untuk formalitas
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiverRegister);
     }
 }
